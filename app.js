2cd15be55445d87e7061f15809784e369209c52c
alexandarp <alexandar.podobnik@gmail.com>
2015-03-24 12:44:39 -0700

    changning to node server

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-var express = require('express');
-
-// Constants
-var DEFAULT_PORT = 5000;
-var PORT = process.env.PORT || DEFAULT_PORT;
-
-// App
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World\n');
-});
-
-app.listen(PORT)
-console.log('Running on http://localhost:' + PORT);
+var http = require('http');  
+http.createServer(function (req, res) {  
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '0.0.0.0');
